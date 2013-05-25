6727c989bdc5a5bb848c3ed2e1f52cedaa5fbc0e
rofl0r <retnyg@gmx.net>
2013-05-25 08:28:03 +0200

    direction.h: add dir name lut

--- direction.h
+++ direction.h
@@ -12,4 +12,20 @@ enum direction {
 	DIR_NO,
 };
 
+static inline const char* dir_name(enum direction dir) {
+	#define DIRNAME(a) [ a ] = #a
+	static const char* dir_names[] = {
+		DIRNAME(DIR_N),
+		DIRNAME(DIR_NW),
+		DIRNAME(DIR_W),
+		DIRNAME(DIR_SW),
+		DIRNAME(DIR_S),
+		DIRNAME(DIR_SO),
+		DIRNAME(DIR_O),
+		DIRNAME(DIR_NO),
+	};
+	#undef DIRNAME
+	return dir_names[dir];
+}
+
 #endif
