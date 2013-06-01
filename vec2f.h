23aaae475483b9581f6d0452664ee2a311889e19
rofl0r <retnyg@gmx.net>
2013-06-01 06:24:51 +0200

    vec2f: constify inline funcs

--- vec2f.h
+++ vec2f.h
@@ -7,24 +7,24 @@ typedef struct { float x, y; } vec2f;
 
 #define VEC(a, b) (vec2f) { .x = a, .y = b }
 
-static inline vec2f velocity(vec2f* from, vec2f* to) {
+static inline vec2f velocity(const vec2f* from, const vec2f* to) {
 	return VEC(to->x - from->x, to->y - from->y);
 }
 
-static inline float veclength(vec2f *v) {
+static inline float veclength(const vec2f *v) {
 	return sqrtf((v->x * v->x) + (v->y * v->y));
 }
 
-static inline float vecdist(vec2f* a, vec2f* b) {
+static inline float vecdist(const vec2f* a, const vec2f* b) {
 	vec2f vel = velocity(a, b);
 	return veclength(&vel);
 }
 
-static inline vec2f vecadd(vec2f* a, vec2f* b) {
+static inline vec2f vecadd(const vec2f* a, const vec2f* b) {
 	return VEC(a->x + b->x, a->y + b->y);
 }
 
-static inline vec2f vecsub(vec2f* a, vec2f* b) {
+static inline vec2f vecsub(const vec2f* a, const vec2f* b) {
 	return VEC(a->x - b->x, a->y - b->y);
 }
 
