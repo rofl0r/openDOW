e6ad81859b71ae78defe33c3d50b51f545c125c6
rofl0r <retnyg@gmx.net>
2013-06-01 06:27:55 +0200

    add tool to convert palpic c files into png

---
+++ palpic2png.c
@@ -0,0 +1,24 @@
+#include "palpic.h"
+#include "players.c"
+#include "leptonica/allheaders.h"
+#include <string.h>
+//RcB: LINK "-llept"
+
+int main() {
+	const struct palpic* f = &players.header;
+	PIX* o = pixCreate(f->width, f->height, 32);
+	prgb* palette = palpic_getpalette(f);
+	prgb* bufptr = (prgb*) o->data;
+	size_t i;
+	for(i = 0; i < f->spritecount; i++) {
+		int x, y;
+		const uint8_t *source = palpic_getspritedata(f, i);
+		for(y = 0; y < palpic_getspriteheight(f); y++) {
+			for(x = 0; x < palpic_getspritewidth(f); x++) {
+				*bufptr++ = palette[*source++];
+			}
+		}
+	}
+	pixWritePng("test.png", o, 0.0);
+	return 0;
+}
