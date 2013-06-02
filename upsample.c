239c5a3b05b225f43b68688c6a1e4a8c877ed512
rofl0r <retnyg@gmx.net>
2013-06-02 23:34:45 +0200

    add naive upsample program
    the theory behind upsampling:
    to scale from 11025 to 44100, the integral scalefactor (N) is 4.
    so each sample in source is converted into N output samples
    as follows:
    sample_t upsampled[N] = { source_sample, 0, 0, 0 };
    1) use the source value
    2) use N-1 0's.
    3) apply some kind of filter on the 4 values
    
    in the current implementation, 3) is skipped.
    it seems to work well to upsample 22050 samples, but not ones with
    lower samplerate.

---
+++ upsample.c
@@ -0,0 +1,71 @@
+#include "../c-flod/backends/wave_format.h"
+#include "../c-flod/flashlib/ByteArray.h"
+
+struct player {
+	int play_waveslot;
+	struct ByteArray wave_streams[1];
+	struct ByteArray out_wave;
+	WAVE_HEADER_COMPLETE wavhdr;
+} playa;
+
+static int handle_overflow(int *sample) {
+	if(*sample > 32767) {
+		*sample = 32767;
+		return 1;
+	} else if (*sample < -32768) {
+		*sample = -32768;
+		return -1;
+	}
+	return 0;
+}
+
+void upsample() {
+	struct ByteArray* mine = &playa.wave_streams[0];
+	struct ByteArray* out = &playa.out_wave;
+	
+	if(!mine->bytesAvailable(mine)) return;
+	
+	size_t i = 0;
+	size_t upsample_factor = 44100 / playa.wavhdr.wave_hdr.samplerate;
+	size_t readbytes = playa.wavhdr.wave_hdr.bitwidth == 8 ? 1 : 2;
+	
+	while(mine->bytesAvailable(mine)) {
+		int16_t sound;
+		size_t c, u;
+		int chan[2];
+		for(c = 0; c < 2; c++) {
+			if(c < playa.wavhdr.wave_hdr.channels) {
+				if(readbytes == 1) chan[c] = ((uint8_t) ByteArray_readByte(mine) - 128) * 256;
+				else chan[c] = ByteArray_readShort(mine);
+				handle_overflow(&chan[c]);
+			} else 
+				chan[c] = chan[c - 1];
+		}
+		for(u = 0; u < upsample_factor; u++) {
+			for(c = 0; c < 2; c++) {
+				if(u == 0) {
+					ByteArray_writeShort(out, chan[c]);
+				} else ByteArray_writeShort(out, 0);
+			}
+		}
+	}
+
+	return;
+}
+
+static char buf[1024* 1024];
+
+int main(int argc, char ** argv) {
+	struct ByteArray *ba = &playa.wave_streams[0];
+	ByteArray_ctor(ba);
+	if(!ByteArray_open_file(ba, argv[1])) return 1; 
+	ByteArray_set_endian(ba, BAE_LITTLE);
+	ByteArray_readMultiByte(ba, (void*) &playa.wavhdr, sizeof(WAVE_HEADER_COMPLETE));
+	struct ByteArray *out = &playa.out_wave;
+	ByteArray_ctor(out);
+	ByteArray_set_endian(out, BAE_LITTLE);
+	ByteArray_open_mem(out, buf, sizeof buf);
+	upsample();
+	ByteArray_dump_to_file(out, "test.raw");
+	
+}
