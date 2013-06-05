1908453ffbc647bb4add2e9ce2d30f203fdca955
rofl0r <retnyg@gmx.net>
2013-06-05 15:24:58 +0200

    add tool to find the biggest in a series of diff images
    ./utils/imgdiff-biggest.pl /tmp/dow-conv/00000059.png /tmp/dow-conv/00000008.png /tmp/dow-conv/00000012.png /tmp/dow-conv/00000016.png /tmp/dow-conv/00000020.png /tmp/dow-conv/00000024.png /tmp/dow-conv/00000028.png /tmp/dow-conv/00000032.png /tmp/dow-conv/00000036.png /tmp/dow-conv/00000040.png /tmp/dow-conv/00000044.png /tmp/dow-conv/00000048.png

---
+++ utils/imgdiff-biggest.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use File::Basename;
+use Cwd 'abs_path';
+my $path = dirname(abs_path($0));
+
+my $inp = shift @ARGV;
+my $x = 99999999999;
+my $y = 99999999999;
+my $wmax = -1;
+my $hmax = -1;
+my $biggest = "";
+for(@ARGV) {
+	my $cmd = $path . "/imgdiff.out $inp $_ /tmp/__aaabbbbxxxx.png";
+	my $res = `$cmd`;
+	if($res =~ /(\d+),(\d+),(\d+),(\d+)/) {
+		my $xi = $1;
+		my $yi = $2;
+		my $wi = $3;
+		my $hi = $4;
+		if($xi < $x) {
+			$x = $xi;
+			$biggest = $_;
+		}
+		if($yi < $y) {
+			$y = $yi;
+			$biggest = $_;
+		}
+		if($wi > $wmax) {
+			$wmax = $wi;
+			$biggest = $_;
+		}
+		if($hi > $hmax) {
+			$hmax = $hi;
+			$biggest = $_;
+		}
+	}
+}
+
+print "biggest: $biggest : $x,$y,$wmax,$hmax\n";
