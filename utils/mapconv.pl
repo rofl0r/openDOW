e73baa879f96f0ef9ab3025d6f2346f7bbd3c128
rofl0r <retnyg@gmx.net>
2013-06-14 18:54:34 +0200

    add tool to convert mapdesigner output into C structs

---
+++ utils/mapconv.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+print "\t.screens = {\n";
+my $i = 0;
+while(<>) {
+	if(/^A/) {
+		print "\t\t\t},\n\t\t},\n" if($i);
+		print "\t\t[$i] = {\n\t\t\t.scrolldir = MS_UP,\n\t\t\t.bg = {\n";
+		$i++;
+		next;
+	}
+	if(/^B/) {
+		print "\t\t\t},\n\t\t\t.fg = {\n";
+		next;
+	}
+	chomp;
+	print "\t\t\t\t{$_},\n";
+}
+print "\t\t\t},\n\t\t},\n\t},\n";
