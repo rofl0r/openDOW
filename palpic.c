9a5397f7e94f2892ba10ff67f529a63c0484992e
rofl0r <retnyg@gmx.net>
2013-06-14 20:50:21 +0200

    enhance blit_sprite() to properly blit OOB sprites
    yay! no more segfaults when moving the mouse out of the window ;)

--- palpic.c
+++ palpic.c
@@ -22,8 +22,27 @@ void blit_sprite(int x_pos, int y_pos, struct vo_desc *video,
 	if(!palette) palette = palpic_getpalette(pic);
 	const uint8_t *bitmap = palpic_getspritedata(pic, spritenum);
 	unsigned int scale_y, scale_x, y, x;
-	unsigned lineoffset = y_pos * (video->pitch / 4);
-	unsigned pixel_start = 0;
+	int x_tl_off = 0, y_tl_off = 0;
+	int x_br_off = 0, y_br_off = 0;
+	if(x_pos >= (int)video->width) return;
+	else if(x_pos < 0) x_tl_off = -x_pos;
+	else if(x_pos + sprite_width * scale >= video->width) {
+		x_br_off = x_pos + sprite_width * scale - video->width;
+		int odd = 0;
+		if(x_br_off%scale)odd=1;
+		x_br_off=x_br_off/scale+odd;
+	}
+	if(y_pos >= (int)video->height) return;
+	else if(y_pos < 0) y_tl_off = -y_pos;
+	else if(y_pos + sprite_height * scale >= video->height) {
+		y_br_off = y_pos + sprite_height * scale - video->height;
+		int odd = 0;
+		if(y_br_off%scale)odd=1;
+		y_br_off=y_br_off/scale+odd;
+	}
+	
+	unsigned lineoffset = (y_pos + y_tl_off) * (video->pitch / 4);
+	unsigned pixel_start = y_tl_off * sprite_width + x_tl_off;
 	static const sdl_rgb_t mask_colors_transp[2] = {
 		[0] = (SRGB_BLACK),
 		[1] = (SRGB_WHITE),
@@ -33,11 +52,11 @@ void blit_sprite(int x_pos, int y_pos, struct vo_desc *video,
 		[1] = (SRGB_BLACK),
 	};
 	const sdl_rgb_t *mask_colors = (pic->flags & PPF_TRANSPARENT ? mask_colors_transp : mask_colors_non_transp);
-	for (y = 0; y < sprite_height; y++) {
+	for (y = y_tl_off; y < sprite_height - y_br_off; y++) {
 		for(scale_y = 0; scale_y < scale; scale_y++) {
-			sdl_rgb_t *ptr = &((sdl_rgb_t *) video->mem)[lineoffset + x_pos];
+			sdl_rgb_t *ptr = &((sdl_rgb_t *) video->mem)[lineoffset + x_pos + x_tl_off];
 			const uint8_t *p = &bitmap[pixel_start];
-			for (x = 0; x < sprite_width; x++) {
+			for (x = x_tl_off; x < sprite_width - x_br_off; x++) {
 				prgb col = palette[*p++];
 				uint32_t mask = mask_colors[(col.val == palette[0].val)].asInt;
 				for(scale_x = 0; scale_x < scale; scale_x++) {
