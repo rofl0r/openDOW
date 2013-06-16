rofl0r/openDOW

commit eacf4aec4cb5e502eddbdc6d35d1e666f4cc3047

advance_animations: fix 2 bugs causing force_redraw to be set
1) if player's anim was set to ANIM_INIT, the next anim was not
   set. this lead to it staying at ANIM_INIT until the player
   was finally moved.
2) only set force_redraw if the anim has actually changed.

spriteview.c
@@ -707,11 +707,12 @@ static int advance_animations(void) {
 	for(i = 0, obj_visited = 0; obj_visited < obj_count && i < OBJ_MAX; i++) {
 		if(!obj_slot_used[i]) continue;
 		struct gameobj *go = &objs[i];
-		if((go->vel.x != 0 || go->vel.y != 0) || (go->objtype != OBJ_P1 && go->objtype != OBJ_P2) || is_death_anim(go->animid)) {
+		if(go->anim_curr == ANIM_STEP_INIT || (go->vel.x != 0 || go->vel.y != 0) || (go->objtype != OBJ_P1 && go->objtype != OBJ_P2) || is_death_anim(go->animid)) {
 			unsigned anim_delay = go->objtype == OBJ_BIG_EXPLOSION ? 8 : 4;
 			if(go->anim_curr == ANIM_STEP_INIT || tickcounter % anim_delay == go->anim_frame) {
+				anim_step last_anim = go->anim_curr;
 				go->anim_curr = get_next_anim_frame(go->animid, go->anim_curr);
-				res = 1;
+				if(last_anim != go->anim_curr) res = 1;
 			}
 		}
 		obj_visited++;

commit 332d4ef5eccf628414701f086714d13cf02785fe

fix regression: screen stayed blank after fullscreen toggle
introduced in eacf4aec4cb5e502eddbdc6d35d1e666f4cc3047
after removing the superfluous screen redraws, one necessary redraw
after the fullscreen switch was missing.

i had to insert a few delays and other shit to make it actually redraw.
when i only inserted need_redraw = 1, the screen stayed black.

spriteview.c
@@ -1158,10 +1158,14 @@ int main() {
 						case SDLK_RETURN:
 							if((sdl_event.key.keysym.mod & KMOD_LALT) ||
 							   (sdl_event.key.keysym.mod & KMOD_RALT)) {
+								fullscreen_active = !fullscreen_active;
 								SDL_WM_ToggleFullScreen(surface);
+								SDL_Delay(1);
 								clear_screen();
 								SDL_UpdateRect(surface,0,0,VMODE_W,VMODE_H);
-								fullscreen_active = !fullscreen_active;
+								SDL_Delay(1);
+								game_tick(1);
+								need_redraw = 1;
 							}
 							break;
 						case SDLK_KP_PLUS: