dd7252db11f87ead5da6ed718bfce0525f053bb7
rofl0r <retnyg@gmx.net>
2013-06-21 23:52:33 +0200

    add music header

---
+++ music.h
@@ -0,0 +1,14 @@
+#ifndef MUSIC_H
+#define MUSIC_H
+
+//RcB: DEP "music/dogsofwar.c"
+extern const unsigned char dogsofwar_dw[];
+extern const unsigned long dogsofwar_dw_size;
+
+enum tune_index {
+	TUNE_FIGHTING = 0,
+	TUNE_MAP = 1,
+	TUNE_HIGHSCORE = 5,
+};
+
+#endif
