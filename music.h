def68e3a5ef4f0605312d9ce8928a7a3942348f3
rofl0r <retnyg@gmx.net>
2013-06-25 19:31:27 +0200

    music.h: add enum members for all available tunes

--- music.h
+++ music.h
@@ -7,8 +7,15 @@ extern const unsigned long dogsofwar_dw_size;
 
 enum tune_index {
 	TUNE_FIGHTING = 0,
-	TUNE_MAP = 1,
-	TUNE_HIGHSCORE = 5,
+	TUNE_MAP,
+	TUNE_2,
+	TUNE_3,
+	TUNE_4,
+	TUNE_HIGHSCORE,
+	TUNE_6,
+	TUNE_7,
+	TUNE_INVALID,
+	TUNE_MAX = TUNE_INVALID
 };
 
 #endif
