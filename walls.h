ae373d61b67d34176a7509897ddece56ee0fc243
rofl0r <retnyg@gmx.net>
2013-07-14 18:27:16 +0200

    introduce wall types (for walls that can be passed by bullets, like fences)

--- walls.h
+++ walls.h
@@ -3,7 +3,13 @@
 
 #include "map.h"
 
-extern const uint8_t walls[MT_MAX][210];
+enum __attribute__ ((__packed__)) walltype {
+	WT_NONE = 0,
+	WT_SOLID = 1,
+	WT_BULLETS_PASS = 2,
+};
+
+extern const enum walltype walls[MT_MAX][210];
 
 //RcB: DEP "walls.c"
 #endif
